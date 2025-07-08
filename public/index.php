84b1d5160f31fb202090dc220dd0c3bce48d0a9b
Oliver Stotzem <5693011+stotzem@users.noreply.github.com>
2025-07-08 07:57:29 +0200

    Add composer

--- public/index.php
+++ public/index.php
@@ -1,20 +1,8 @@
 <?php
 
-namespace Stotzem;
+require __DIR__ . '/../vendor/autoload.php';
 
-spl_autoload_register(function (string $class) {
-    $prefix = 'Stotzem\\';
-    $base_dir = __DIR__ . '/../src/';
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
-    $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-});
+use Stotzem\Kernel;
 
 $kernel = new Kernel();
 $kernel->run();
